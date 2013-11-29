80128ffdc037c63f92ebe39a252981cae6e9552c
Sean Edwards <stedwards87@gmail.com>
2013-11-29 18:12:38 -0500

    More detailed example.

--- cpp/future.cpp
+++ cpp/future.cpp
@@ -1,10 +1,21 @@
 #include <functional>
 #include <future>
 #include <iostream>
+#include <sstream>
 
 /* An idea for continuing computation with C++11 futures.
  * Compile with: clang++ -std=c++11 -stdlib=libc++ future.cpp
  */
+
+std::string format_result(const std::string& str) {
+	return "Result is: " + str;
+}
+
+std::string to_string(int x) {
+	std::stringstream str;
+	str << x;
+	return str.str();
+}
 
 int add_one(int x) {
 	return x + 1;
@@ -16,7 +27,7 @@ auto operator<<(std::future<T> fut, F func) -> std::future<decltype(func(fut.get
 }
 
 int main() {
-	std::cout << (std::async(add_one, 1) << add_one << add_one).get() << std::endl;
+	std::cout << (std::async(add_one, 1) << add_one << add_one << to_string << format_result).get() << std::endl;
 	return 0;
 }
 
